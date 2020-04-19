2105f93d262c4f77408ceeb6dd3f5b59d0f1293a
Arthur Barbosa <arthurbruno03@gmail.com>
2020-04-19 12:34:45 -0400

    METHOD GET and POST repositories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: "Passaro app",
+      url: 'https://github.com/arthurBarbosa/passaro-app',
+      techs: ['Angular', 'Typescript']
+    
+    })
+    const repository  = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +30,14 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(1)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
